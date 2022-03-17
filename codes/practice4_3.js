0d7ad65560fd10431acf0e5e507e6345ecb45055
yuma <ikeoku.y@gmail.com>
2022-03-17 09:23:05 +0900

    practice4-3完成

--- codes/practice4_3.js
+++ codes/practice4_3.js
@@ -9,7 +9,6 @@ function arrayToList(array) {
 function  listToArray(list) {
     let array = [];
     for (let i = list; i !== null; i = i.rest) {
-        console.log(i)
         array.push(i.value);
     }
     return array;
@@ -31,18 +30,16 @@ function nth(list, num) {
 // 再帰
 {
     function nth(list, num) {
-        console.log(list);
-        console.log(list.value);
         if (num === 0) return list.value;
         else if (list === null) return undefined;
-        else nth(list.rest, num - 1);
+        else return nth(list.rest, num - 1);
     }
 
     console.log(nth(arrayToList([10, 20, 30]), 1));
     // → 20
 }
 
-/*
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 
@@ -54,4 +51,4 @@ console.log(prepend(10, prepend(20, null)));
 
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
-*/
+
