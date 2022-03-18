cabaff488652ab5ce140cf0ab11db039301ee2bd
eatplaynap <eatplaynap329@gmail.com>
2022-03-18 09:40:51 +0900

    厳格な等価性比較作成コード

--- codes/practice4/practice4_4.js
+++ codes/practice4/practice4_4.js
@@ -1,21 +1,45 @@
-let obj = {here: {is: "an"}, object: 2};
+let obj = { here: { is: "an" }, object: 2 };
 
+// valueに複数ネストしたものがある場合、正しく動かない
 function deepEqual(obj1, obj2) {
-  result = []
   if (Object.keys(obj1).length !== Object.keys(obj2).length) return false
-  else {
-    for (let i = 0; i < Object.keys(obj1).length; i++) {
-      result.push(Object.keys(obj1)[i] === Object.keys(obj2)[i])
-    }
-    return result.every(e => e)
+  if (Object.values(obj1).some(value => typeof (value) === 'object' && value !== null)) {
+    // 2つのObjectのネストした中が等価かどうか比べていく
+    const key = Object.keys(obj1).filter(key => typeof obj1[key] === 'object')
+    return deepEqual(obj1[key], obj2[key])
+  } else {
+    // 外側のプロパティが等しいかどうかを見る
+    return Object.keys(obj1).every((key, idx) => key === Object.keys(obj2)[idx])
   }
 }
 
+// 模範解答 ↑での問題もクリア済み
+function deepEqual(a, b) {
+  if (a === b) return true;
+
+  if (a == null || typeof a != "object" ||
+    b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+
 console.log(deepEqual(obj, obj));
 // → true
-console.log(deepEqual(obj, {here: 1}))
+console.log(deepEqual(obj, { here: 1 }))
 // → false
-console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, { here: 1, object: 2 }));
 // → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
 // → true
+console.log(deepEqual(obj, { here: { is: "an" }, object: { is: "an" } }))
+// → false
+// 未対応
