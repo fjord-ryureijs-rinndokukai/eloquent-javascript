0b4dc4bc1a6aec5051155c03358a37fd01ae5288
yuma <ikeoku.y@gmail.com>
2022-05-16 08:45:18 +0900

    roadsモジュールを解いた

---
+++ codes/practice10/practice10_2.js
@@ -0,0 +1,14 @@
+// Add dependencies and exports
+const {buildGraph} = require("./packages_chapter_10");
+
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+exports.roadGraph = buildGraph(roads.map(r => r.split("-")));
