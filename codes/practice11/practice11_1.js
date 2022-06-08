bf0eb974b239f6cbebbbbe6c69a69f073eefc02e
Shohei Umemoto <cafedomancer@gmail.com>
2022-06-09 08:55:36 +0900

    Update practice11_1.js

--- codes/practice11/practice11_1.js
+++ codes/practice11/practice11_1.js
@@ -1,19 +1,20 @@
 async function locateScalpel(nest) {
   // Your code here.
-  let curr = null;
-  let next = network(nest)[0];
-  
+  let curr = null
+  let next = network(nest)[0]
+
   while (true) {
-    console.log(`${curr} -> ${next}`);
-    curr = next;
-    next = await anyStorage(nest, curr, 'scalpel');
-    if (curr === next) break;
+    curr = next
+    next = await anyStorage(nest, curr, 'scalpel')
+    if (curr === next) return curr
   }
 }
 
 function locateScalpel2(nest) {
-  // Your code here.
+  let curr = nest.name
+  const loop = () => anyStorage(nest, curr, 'scalpel').then((next) => (curr === next ? curr : loop(next)))
+  loop(curr)
 }
 
-locateScalpel(bigOak).then(console.log);
+locateScalpel(bigOak).then(console.log)
 // → Butcher Shop
