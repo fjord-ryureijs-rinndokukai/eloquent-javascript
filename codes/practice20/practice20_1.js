e2811936c1a96ef8fc65468775699beda056a30f
yatsuhashi168 <kurimi777luk@gmail.com>
2022-10-04 09:59:11 +0900

    20章2問目終了

--- codes/practice20/practice20_1.js
+++ codes/practice20/practice20_1.js
@@ -1,35 +1,35 @@
-const fs = require("fs").promises;
-const Path = require("path");
+const fs = require('fs').promises
+const Path = require('path')
 
-const pattern = new RegExp(process.argv[2]);
-const paths = process.argv.slice(3);
-
-paths.forEach(async (path) => {
-  const stats = await fs.stat(path);
-  if (stats.isDirectory()) {
-    const files = await fs.readdir(path);
-    console.dir(files);
-  } else {
-    const content = await fs.readFile(path, "utf-8");
-    if (pattern.test(content)) {
-      console.log(path);
-    }
-  }
-});
+const pattern = new RegExp(process.argv[2])
+const paths = process.argv.slice(3)
+const list = []
 
 const readdirRecursively = async (dir) => {
-  const paths = (await fs.readdir(dir)).map((path) => Path.join(dir, path));
-  const list = [];
+  const paths = (await fs.readdir(dir)).map((path) => Path.join(dir, path))
+  const list = []
 
   for (const path of paths) {
-    const stats = await fs.stat(path);
+    const stats = await fs.stat(path)
     if (stats.isDirectory()) {
-      const dir_ = await readdirRecursively(path);
-      list.push(...dir_);
+      const dir_ = await readdirRecursively(path)
+      list.push(...dir_)
     } else {
-      list.push(path);
+      list.push(path)
     }
   }
 
-  return list;
-};
+  return list
+}
+
+(async () => {
+  for (const path of paths) {
+    list.push(...(await readdirRecursively(path)))
+  }
+  for (const file of list) {
+    const content = await fs.readFile(file, 'utf8');
+    if (pattern.test(content)) {
+      console.log(file);
+    }
+  }
+})();
--- codes/practice20/practice20_2.js
+++ codes/practice20/practice20_2.js
@@ -1,109 +1,108 @@
-const { createServer } = require("http");
-const { stat, readdir, rmdir, mkdir, unlink } = require("fs").promises;
-const { createReadStream } = require("fs");
-const { createWriteStream } = require("fs");
-const mime = require("mime");
-const { parse } = require("url");
-const { resolve, sep } = require("path");
+const { createServer } = require('http')
+const { stat, readdir, rmdir, mkdir, unlink } = require('fs').promises
+const { createReadStream } = require('fs')
+const { createWriteStream } = require('fs')
+const mime = require('mime')
+const { parse } = require('url')
+const { resolve, sep } = require('path')
 
-const baseDirectory = process.cwd();
+const baseDirectory = process.cwd()
 
-function urlPath(url) {
-  let { pathname } = parse(url);
-  let path = resolve(decodeURIComponent(pathname).slice(1));
+function urlPath (url) {
+  let { pathname } = parse(url)
+  let path = resolve(decodeURIComponent(pathname).slice(1))
   if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
-    throw { status: 403, body: "Forbidden" };
+    throw { status: 403, body: 'Forbidden' }
   }
-  return path;
+  return path
 }
 
-const methods = Object.create(null);
+const methods = Object.create(null)
 
 methods.GET = async function (request) {
-  let path = urlPath(request.url);
-  let stats;
+  let path = urlPath(request.url)
+  let stats
   try {
-    stats = await stat(path);
+    stats = await stat(path)
   } catch (error) {
-    if (error.code != "ENOENT") throw error;
-    else return { status: 404, body: "File not found" };
+    if (error.code != 'ENOENT') throw error
+    else return { status: 404, body: 'File not found' }
   }
   if (stats.isDirectory()) {
-    return { body: (await readdir(path)).join("\n") };
+    return { body: (await readdir(path)).join('\n') }
   } else {
-    return { body: createReadStream(path), type: mime.getType(path) };
+    return { body: createReadStream(path), type: mime.getType(path) }
   }
-};
+}
 
-function pipeStream(from, to) {
+function pipeStream (from, to) {
   return new Promise((resolve, reject) => {
-    from.on("error", reject);
-    to.on("error", reject);
-    to.on("finish", resolve);
-    from.pipe(to);
-  });
+    from.on('error', reject)
+    to.on('error', reject)
+    to.on('finish', resolve)
+    from.pipe(to)
+  })
 }
 
 methods.PUT = async function (request) {
-  let path = urlPath(request.url);
-  await pipeStream(request, createWriteStream(path));
-  return { status: 204 };
-};
+  let path = urlPath(request.url)
+  await pipeStream(request, createWriteStream(path))
+  return { status: 204 }
+}
 
 methods.DELETE = async function (request) {
-  let path = urlPath(request.url);
-  let stats;
+  let path = urlPath(request.url)
+  let stats
   try {
-    stats = await stat(path);
+    stats = await stat(path)
   } catch (error) {
-    if (error.code != "ENOENT") throw error;
-    else return { status: 204 };
+    if (error.code != 'ENOENT') throw error
+    else return { status: 204 }
   }
-  if (stats.isDirectory()) await rmdir(path);
-  else await unlink(path);
-  return { status: 204 };
-};
-
+  if (stats.isDirectory()) await rmdir(path)
+  else await unlink(path)
+  return { status: 204 }
+}
 
 // NOTE: 練習問題
 
 methods.MKCOL = async (request) => {
-  // return { status: 200, body: "This is MKCOL method" };
-  let path = urlPath(request.url);
-  let stats;
+  let path = urlPath(request.url)
+  let stats
   try {
-    stats = await stat(path);
-    return { status: 200, body: "見つかったよ" };
-  } catch (error) {
-    // NOTE: ディレクトリが見つからないときは、mkdirでディレクトリを作成する
-    if (error.code != "ENOENT") {
-      throw error;
+    stats = await stat(path)
+    if (stats.isDirectory()) {
+      return { status: 204 }
     } else {
-      await mkdir(path);
-      return { status: 204 };
+      return { status: 400, body: 'Bad Request'}
+    }
+  } catch (error) {
+    if (error.code != 'ENOENT') {
+      throw error
+    } else {
+      await mkdir(path)
+      return { status: 204 }
     }
   }
-  // TODO: ディレクトリが存在する場合は、204レスポンスを返す
-  // TODO: ディレクトリではないファイルが存在する場合は、400 Bad Requestを返す
-};
+}
 
-async function notAllowed(request) {
+async function notAllowed (request) {
   return {
     status: 405,
     body: `Method ${request.method} not allowed.`,
-  };
+  }
 }
 
 createServer((request, response) => {
-  let handler = methods[request.method] || notAllowed;
+  let handler = methods[request.method] || notAllowed
   handler(request)
     .catch((error) => {
-      if (error.status != null) return error;
-      return { body: String(error), status: 500 };
+      if (error.status != null) return error
+      return { body: String(error), status: 500 }
     })
-    .then(({ body, status = 200, type = "text/plain" }) => {
-      response.writeHead(status, { "Content-Type": type });
-      if (body && body.pipe) body.pipe(response);
-      else response.end(body);
-    });
-}).listen(8000);
+    .then(({ body, status = 200, type = 'text/plain' }) => {
+      response.writeHead(status, { 'Content-Type': type })
+      if (body && body.pipe) body.pipe(response)
+      else response.end(body)
+    })
+}).listen(8000)
